b2b544b34dda9660d250dc8f5ea4fa2e7634730c
arina <awwwrina@gmail.com>
2022-02-24 01:48:00 +0300

    delete employee

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,26 +8,45 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css';
 
-function App() {
-    const data = [
-        {name: 'Annie Leonhart', salary: 900, id: 1},
-        {name: 'Ymir Reiss', salary: 2000, id: 2},
-        {name: 'Hange Zoe', salary: 3000, id: 3}
-    ]
-    return (
-        <div className="app">
-            <AppInfo/>
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'Annie Leonhart', salary: 900, id: 1},
+                {name: 'Ymir Reiss', salary: 2000, id: 2},
+                {name: 'Hange Zoe', salary: 3000, id: 3}
+            ]
+        }
+    }
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+
+    render() {
+
+        return (
+            <div className="app">
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployeesList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+    
+                <EmployeesAddForm/>
             </div>
-
-            <EmployeesList data={data}/>
-
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
