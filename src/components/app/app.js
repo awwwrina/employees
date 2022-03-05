2bcadadc5ef7e85af29dc20b8d24a5585279fafc
arina <awwwrina@gmail.com>
2022-03-05 11:52:05 +0300

    search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,8 @@ class App extends Component {
                 {name: 'Annie Leonhart', salary: 900, increase: false, like: true, id: 1},
                 {name: 'Ymir Reiss', salary: 2000, increase: true, like: false, id: 2},
                 {name: 'Hange Zoe', salary: 3000, increase: false, like: false, id: 3}
-            ]
+            ],
+            term: ''
         }
     }
     deleteItem = (id) => {
@@ -53,9 +54,24 @@ class App extends Component {
         }))
     }
 
+    searchEmp = (items,term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term})
+    }
     render() {
+        const {data, term} = this.state;
         const employees = this.state.data.length;
         const increased = this.state.data.filter(item => item.increase).length
+        const visibleData = this.searchEmp(data, term);
         return (
             <div className="app">
                 <AppInfo
@@ -63,12 +79,13 @@ class App extends Component {
                     increased={increased}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
+                    <SearchPanel
+                        onUpdateSearch={this.onUpdateSearch}/>
                     <AppFilter/>
                 </div>
     
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
 
