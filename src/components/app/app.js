81a16a4eb3e8c5b9b1580ef4045af9449adb3eba
arina <awwwrina@gmail.com>
2022-03-05 10:25:29 +0300

    validation form

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -13,9 +13,9 @@ class App extends Component {
         super(props);
         this.state = {
             data: [
-                {name: 'Annie Leonhart', salary: 900, id: 1},
-                {name: 'Ymir Reiss', salary: 2000, id: 2},
-                {name: 'Hange Zoe', salary: 3000, id: 3}
+                {name: 'Annie Leonhart', salary: 900, increase: false, like: true, id: 1},
+                {name: 'Ymir Reiss', salary: 2000, increase: true, like: false, id: 2},
+                {name: 'Hange Zoe', salary: 3000, increase: false, like: false, id: 3}
             ]
         }
     }
@@ -26,13 +26,41 @@ class App extends Component {
             }
         })
     }
+    addItem = (name, salary) =>{
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            like: false,
+            id: Math.floor(Math.random() * (1000 - 3) + 3)
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return{
+                data: newArr
+            }    
+        });
+    }
 
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
 
     render() {
-
+        const employees = this.state.data.length;
+        const increased = this.state.data.filter(item => item.increase).length
         return (
             <div className="app">
-                <AppInfo/>
+                <AppInfo
+                    employees={employees}
+                    increased={increased}/>
     
                 <div className="search-panel">
                     <SearchPanel/>
@@ -41,9 +69,11 @@ class App extends Component {
     
                 <EmployeesList 
                     data={this.state.data}
-                    onDelete={this.deleteItem}/>
-    
-                <EmployeesAddForm/>
+                    onDelete={this.deleteItem}
+                    onToggleProp={this.onToggleProp}/>
+
+                <EmployeesAddForm
+                    onAdd={this.addItem}/>
             </div>
         );
     }
--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+
 import './employees-add-form.css'
 
 class EmployeesAddForm extends Component {
@@ -6,30 +7,55 @@ class EmployeesAddForm extends Component {
         super(props);
         this.state = {
             name: '',
-            salary: ''
+            salary: '',
+            warning: ''
         }
     }
+
     onValueChange = (e) => {
         this.setState({
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value, 
+            warning: ''
         })
     }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        if (this.state.name.length < 3 || this.state.salary.length < 3)  {
+           this.setState({
+               warning: 'Имя и/или зарплата не может быть короче двух символов'
+           })
+        } else {
+            this.props.onAdd(this.state.name, this.state.salary);
+            this.setState({
+                name: '',
+                salary: ''
+            })
+        }
+    }
+
+    getClassInput = () => {
+        const {warning} = this.state;
+        return warning ? "form-control new-post-label warning-outline" : "form-control new-post-label"
+    }
     render() {
-        const {name, salary} = this.state;
+        const {name, salary, warning} = this.state;
+        
         return (
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit = {this.onSubmit}>
                     <input type="text"
-                        className="form-control new-post-label"
+                        className={this.getClassInput()}
                         placeholder="Имя"
                         name="name"
                         value={name}
                         onChange={this.onValueChange} />
                     <input type="number"
-                        className="form-control new-post-label"
+                        className={this.getClassInput()}
                         placeholder="Зарплатушка" 
                         name="salary"
                         value={salary}
@@ -37,8 +63,10 @@ class EmployeesAddForm extends Component {
                         />
     
                     <button type="submit"
-                            className="btn btn-outline-light">Добавить</button>
+                            className="btn btn-outline-light"
+                            >Добавить</button>
                 </form>
+                {warning ? <div className="warning">{warning}</div> : null}
             </div>
         );
     }
